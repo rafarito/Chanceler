4ab323e61264b48747fea08ef7416869f6eaf8a5
rafarito <rafaritogames@gmail.com>
2023-03-17 14:51:25 -0300

    desenvolvimento do comando de comissoes

--- comandos/utilidades/comissoes.js
+++ comandos/utilidades/comissoes.js
@@ -13,22 +13,60 @@ module.exports = {
         required: true,
         choices: [
             {
-                name: "membros",
-                value: "comi_membros"
+                name: "encremento de novos membros",
+                value: "com_membros",
             },
             {
-                name: "dinheiro",
-                value: "comi_dinheiro"
-            }
+                name: "Hospitalaria",
+                value: "com_hosp",
+            },
+            {
+                name: "Auditoria",
+                value: "com_audi",
+            },
+            {
+                name: "Entreterimento",
+                value: "com_entre",
+            },
+            {
+                name: "Finanças",
+                value: "com_fin",
+            },
         ]
     },
   ],
 
   run: async (client, interaction) => {
 
+    let com = interaction.options.getString("comissão");
+
+    switch (com){
+      case "com_membros":
+        var titulo = `Comissão de Encremento de Novos Membros`
+        var descricao = `Olá ${interaction.user}\n\nEsta comissão é responsavel por realizar campanhas para aderir novos membros, se você conhece alguem que quer indicar, entre em contato com membros da comissão ou com seu preseidente ${client.users.cache.get(DONO)}.`
+      break;
+      case "com_hosp":
+        var titulo = `Comissão de Hospitalaria`
+        var descricao = `Olá ${interaction.user}\n\nEsta comissão é responsavel` // ainda não sei descrever exatamente essa comissão
+      break;
+      case "com_audi":
+        var titulo = `Comissão de Auditoria`
+        var descricao = `c` // ainda não sei descrever exatamente essa comissão
+      break;
+      case "com_entre":
+        var titulo = `Comissão de Entreterimento`
+        var descricao = `Olá ${interaction.user}\n\nEstá comissão fica responsavel por organizar as atividades de entreterimento do capitulo, se você possui alguma duvida ou sugestão, entre em contato com os membros da comissão ou com o presidente \`Felipe Sérgio\`.`
+      break;
+      case "com_fin":
+        var titulo = `Comissão de Finanças`
+        var descricao = `f` // ainda não sei descrever exatamente essa comissão
+      break;
+    }
+
     let embed = new Discord.EmbedBuilder()
     .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
-    .setDescription(`Olá ${interaction.user}\n\nNossa ordem precisa de renovação constante, então se você conhece garotos que na sua concepção, podem ser iniciados na ordem, encaminhe seu contato para membros da comissão de novos membros ou o presidente da comissão ${client.users.cache.get(DONO)}. \n\nContamos com você!!!`)
+    .setTitle(titulo)
+    .setDescription(descricao)
     .setColor("Green");
 
     interaction.reply({ embeds: [embed] })
